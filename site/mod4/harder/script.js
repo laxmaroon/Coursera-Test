a1631d630dfd9a425e7f74875830f560129ea295
laxmaroon <102042850+laxmaroon@users.noreply.github.com>
2022-07-22 20:27:35 +0530

    Sixth commit

--- site/mod4/harder/script.js
+++ site/mod4/harder/script.js
@@ -1,12 +1,12 @@
 (function(){
   var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
   for(var i=0; i< names.length; i++){
-    var firstLetter = names.charAt(0);
-  if (names.charAt(0)==="j"|| names.charAt(0)==="J") {
+    var firstLetter = names[i].charAt(0).toLowerCase();
+    if (firstLetter==="j") {
      byeSpeaker.speak(names[i]);
-  } 
-  else {
+    } 
+    else {
     helloSpeaker.speak(names[i]);
-   }
+    }
   }
 })();
