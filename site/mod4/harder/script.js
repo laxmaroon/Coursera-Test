55b0c432e8aa99c5544c29e2d6fa96655202d2d3
laxmaroon <102042850+laxmaroon@users.noreply.github.com>
2022-07-22 19:20:11 +0530

    Third commit

--- site/mod4/harder/script.js
+++ site/mod4/harder/script.js
@@ -1,12 +1,13 @@
 (function(window){
-var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+  var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
- for(var i=0; i< names.length; i++){
-  var firstLetter = names.charAt(0);
-  if (names.charAt(0)==="j"|| name.charAt(0)==="J") {
+  for(var i=0; i< names.length; i++){
+   var firstLetter = names.charAt(0);
+   if (names.charAt(0)==="j"|| name.charAt(0)==="J") {
     byeSpeaker.speak();
-  } else {
+   } 
+  else {
     helloSpeaker.speak();
+   }
   }
-}
 })(window);
